c90844dbeaa7be1ba78837c6d5401eab32b6873b
Monkey02Psycho <monkeypsycho03@gmail.com>
2019-02-26 12:27:01 -0700

    Added driving code

---
+++ Drive.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    public float carSpeed;
+    private bool turningLeft;
+    private bool turningRight;
+
+    // Start is called before the first frame update
+    void Start() {
+        carSpeed = 5.0f;
+        turningLeft = false;
+        turningRight = false;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) { turningLeft = true; }
+        if (Input.GetKeyDown(KeyCode.D)) { turningRight = true; }
+    }
+    void FixedUpdate()
+    {
+        
+    }
+}
