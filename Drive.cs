1f29d20d2c3771674dc6140c6b60eca2c78ec934
Monkey02Psycho <monkeypsycho03@gmail.com>
2019-02-27 14:36:19 -0700

    edited drive code

--- Drive.cs
+++ Drive.cs
@@ -7,12 +7,15 @@ public class Drive : MonoBehaviour
     public float carSpeed;
     private bool turningLeft;
     private bool turningRight;
+    private Rigidbody rb;
+    private int i = 1;
 
     // Start is called before the first frame update
     void Start() {
         carSpeed = 5.0f;
         turningLeft = false;
         turningRight = false;
+        rb = GetComponent<Rigidbody> ();
     }
 
     // Update is called once per frame
@@ -22,6 +25,13 @@ public class Drive : MonoBehaviour
     }
     void FixedUpdate()
     {
-        
+        if (turningLeft == true)
+        {
+            i++;
+            Vector3 rotationVector = new Vector3(transform.rotation.x, transform.rotation.y + 1 * carSpeed + i, transform.rotation.z);
+            Quaternion rotation = Quaternion.Euler(rotationVector);
+            // rotates the object then when you move forward you go that way
+            rb.rotation = rotation;
+        }  
     }
 }
