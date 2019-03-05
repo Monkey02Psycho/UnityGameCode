1deddb2c507b132b86e331b8451758d9145ab2aa
Monkey02Psycho <monkeypsycho03@gmail.com>
2019-03-05 12:52:35 -0700

    Add files via upload

--- Drive.cs
+++ Drive.cs
@@ -4,34 +4,24 @@ using UnityEngine;
 
 public class Drive : MonoBehaviour
 {
-    public float carSpeed;
-    private bool turningLeft;
-    private bool turningRight;
-    private Rigidbody rb;
-    private int i = 1;
+
 
     // Start is called before the first frame update
     void Start() {
-        carSpeed = 5.0f;
-        turningLeft = false;
-        turningRight = false;
-        rb = GetComponent<Rigidbody> ();
+
     }
 
     // Update is called once per frame
     void Update() {
-        if (Input.GetKeyDown(KeyCode.A)) { turningLeft = true; }
-        if (Input.GetKeyDown(KeyCode.D)) { turningRight = true; }
+
     }
     void FixedUpdate()
     {
-        if (turningLeft == true)
-        {
-            i++;
-            Vector3 rotationVector = new Vector3(transform.rotation.x, transform.rotation.y + 1 * carSpeed + i, transform.rotation.z);
-            Quaternion rotation = Quaternion.Euler(rotationVector);
-            // rotates the object then when you move forward you go that way
-            rb.rotation = rotation;
-        }  
+
+    }
+
+    private void goToPoint(Vector3 point, Transform position)
+    {
+        position.position = new Vector3(transform.position)
     }
 }
